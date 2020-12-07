aaf77c5b670e46a2e9fc50f22ae0171f3e536131
Victor Åkesson <Victorakesson@gmail.com>
2020-12-07 14:05:50 +0100

    Klart

--- 48/1/script.js
+++ 48/1/script.js
@@ -1,3 +1,10 @@
 // Deklarera en variabel name och ge den värdet av ditt namn
 
 // Skriv en for-loop som loggar ut "Hejsan <ditt namn här>!!" 500 gånger
+
+const name = "Victor"
+
+let i = 0 
+while(i < 500) {
+console.log(`Hejsan ${name}!!`)
+}
