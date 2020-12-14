028f225684efa84da2bf6682789e317d1a0a0947
Victor Åkesson <Victorakesson@gmail.com>
2020-12-14 12:57:45 +0100

    Vecka 49 del 2

---
+++ 49/2/script.js
@@ -0,0 +1,15 @@
+let str1 = ''
+for (let i = 0; i < 10; i++) {
+    str1 = str1 + '*'.repeat(i)
+    str1 = str1 + '\n' // Lägg till en radbrytning
+}
+console.log(str1)
+
+
+
+let str2 = ''
+for (let i = 0; i < 5; i++) {
+    str2 = str2 + '* '.repeat(5)
+    str2 = str2 + '\n'
+}
+console.log(str2)
